5a22e97703173bae2df26095b0beac89896a7d77
Crowdbotics <team@crowdbotics.com>
2022-12-16 15:55:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gallop_social_37918",
+  url: "https://gallop_social_37918.botics.co",
+  api: "https://gallop_social_37918.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
